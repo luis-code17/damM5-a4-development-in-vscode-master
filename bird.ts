ac00cf1bfb037ca8bbe39aa26ccab7efb9d75706
luis-code17 <146761507+luis-code17@users.noreply.github.com>
2023-10-27 23:23:00 +0200

    Initial commit

---
+++ bird.ts
@@ -0,0 +1,11 @@
+export class Bird {
+    Birdname: string;
+    Wingspan: number;
+    eggsize: string;
+  
+    constructor(Birdname: string, Wingspan: number, eggsize: string) {
+      this.Birdname = Birdname;
+      this.Wingspan = Wingspan;
+      this.eggsize = eggsize;
+    }
+}
